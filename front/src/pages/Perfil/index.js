2306d4bf72e2f29c0766c91b21e7fce1116da80f
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-11 19:35:49 -0300

    some changes
    a

--- front/src/pages/Perfil/index.js
+++ front/src/pages/Perfil/index.js
@@ -2,24 +2,43 @@ import './style.css';
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-function Perfil(){
+function Perfil() {
 
     const [user, setUser] = useState({});
-    const {id} = useParams();
-    
-    useEffect(()=>{
+    const { id } = useParams();
+
+    useEffect(() => {
         // console.log(id);
-        async function loadUser(){
+        async function loadUser() {
             const response = await api.get(`user/${id}`);
             setUser(response.data);
         }
         loadUser();
     }, [])
 
-    return(
+    return (
         <div className='container'>
-            {JSON.stringify(user)}
+            <div className='left-area'>
+                <img className="user-logo" src={require('../../assets/user-logo.png')} />
+                <div className='user-info'>{user.nome} <br />{user.email} <br />{user.departamento}</div>
+            </div>
+            <div className='right-area'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+            </div>
         </div>
     )
 
