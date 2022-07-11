370f7360f774404440c074ab7c0315196e1a5984
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-11 18:09:25 -0300

    Exemplo de retorno de dados
    Exemplo de retorno de dados

--- front/src/pages/Perfil/index.js
+++ front/src/pages/Perfil/index.js
@@ -1,13 +1,28 @@
 import './style.css';
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { useParams } from 'react-router-dom';
 
 function Perfil(){
+
+    const [user, setUser] = useState({});
+    // const {id} = useParams();
+    
+    useEffect(()=>{
+        // console.log(id);
+        async function loadUser(){
+            const response = await api.get(`user/62bf48a14e84058e682f69b6`);
+            setUser(response.data);
+        }
+        loadUser();
+    }, [])
+
     return(
-        <div>
-            Página de Perfil
+        <div className='container'>
+            {JSON.stringify(user)}
         </div>
     )
+
 }
 
 export default Perfil;
