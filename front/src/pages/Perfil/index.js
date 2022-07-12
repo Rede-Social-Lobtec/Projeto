fd8b65f6d812a70139e3ec24ae83653ed7967d58
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-12 14:57:04 -0300

    Atualizações no perfil
    Atualizações no perfil

--- front/src/pages/Perfil/index.js
+++ front/src/pages/Perfil/index.js
@@ -4,34 +4,55 @@ import api from '../../services/api';
 import { useParams } from 'react-router-dom';
 
 import Header from '../../components/Header';
+import Card from '../../components/Card';
 
 function Perfil() {
 
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState([{}]);
     const { id } = useParams();
 
     useEffect(() => {
         // console.log(id);
         async function loadUser() {
-            const response = await api.get(`user/${id}`);
-            setUser(response.data);
+            await api.get(`user/${id}`)
+                .then((res) => {
+                    setUser(res.data);
+                })
         }
         loadUser();
-    }, [])
+        console.log(user.seguindo);
+    }, []);
 
     return (
         <div className='container'>
-            {/* <Header/> */}
-            <div className='left-area'>
-                <img className="user-logo" src={require('../../assets/user-logo.png')} />
-                <div className='user-info'>{user.nome} <br />{user.email} <br />{user.departamento}</div>
+            <div className='content1'>
+                <img src={require('../../assets/user-logo.png')} />
+                <h2>{user.nome}</h2>
             </div>
-            <div className='right-area'>
-
+            <div className='content2'>
+                <span>Departamento: {user.departamento}</span>
+                {/* <span>Cargo: {user.cargo}</span> */}
             </div>
+            <div className='content3'>
+                <ul>
+                    {user.seguindo.map((item) => {
+                        return (
+                            <li key={item._id}>
+                                <div className='card'>
+                                    <div className='card-body'>
+                                        <img src={require('../../assets/user-logo.png')} />
+                                        <h2>seguindo1</h2>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className='content4'>content4</div>
+            <div className='content5'>content5</div>
         </div>
     )
-
 }
 
 export default Perfil;
