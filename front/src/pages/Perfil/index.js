0223b2c410479b7bfd6938de8de1af8486ad4e6d
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-12 00:02:08 -0300

    Ajustes
    Tirando o bootstrap

--- front/src/pages/Perfil/index.js
+++ front/src/pages/Perfil/index.js
@@ -2,8 +2,6 @@ import './style.css';
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 function Perfil() {
 
@@ -26,17 +24,6 @@ function Perfil() {
                 <div className='user-info'>{user.nome} <br />{user.email} <br />{user.departamento}</div>
             </div>
             <div className='right-area'>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
 
             </div>
         </div>
