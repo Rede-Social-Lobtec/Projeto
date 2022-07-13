ae05f4df65a93bdd00ca1f251e34f267b39285d1
TimoteoParise <109082315+TimoteoParise@users.noreply.github.com>
2022-07-13 14:14:32 -0300

    home

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,11 +1,62 @@
-import './style.css';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './styleHome.css';
+import nome from '../../assets/escrita_lobtec.png';
 
-function Home(){
-    return(
-        <div>
-            Página Home
+
+function Login() {
+    /*const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');*/
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        Auth();
+    }
+
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID token " + response.credential);
+    }
+
+    async function Auth() {
+
+        var body = {email: '', senha: ''};
+        await api.post('/auth', body)
+        .then((res)=>{
+          localStorage.setItem("token", res.data.token);
+        })
+    }
+
+
+    return (
+        <div className="Hcontainer-center">
+            <div className="Hlogin">
+                </div>
+
+                <div className="Hlogin-img">
+                    <img src={nome} alt="HSistema nome" />
+
+
+                <form onSubmit={handleSubmit} className="Hform-login">
+                    <h1>Bem-Vindo(a) a nossa rede social corporativa</h1>
+                    <Link to="/login"> Login </Link>                    
+                    <Link to="/Cadastro"> Cadastrar </Link>
+
+                </form>
+
+                <hr className='Hdivisor' />
+
+               
+                <div id="signInDiv"></div>
+
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home;
+export default Login;
+
+
+
+
+
