742491428f60659179d7021a107f62d3e93fe9b5
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-18 11:58:56 -0300

    Página de admin finalizada
    Página de admin finalizada

--- front/src/pages/Admin/index.js
+++ front/src/pages/Admin/index.js
@@ -16,6 +16,9 @@ function Admin() {
     const [formOpen, setFormOpen] = useState(false);
     const [deleteOpen, setDeleteOpen] = useState(false);
     const [updateOpen, setUpdateOpen] = useState(false);
+    const [createGOpen, setcreateGOpen] = useState(false);
+    const [deleteGOpen, setDeleteGOpen] = useState(false);
+    const [updateGOpen, setUpdateGOpen] = useState(false);
     const [logsOpen, setLogsOpen] = useState(false);
     const [logsRes, setLogsRes] = useState(null);
     const [temaOpen, setTemaOpen] = useState(false);
@@ -23,6 +26,10 @@ function Admin() {
     const [data, setData] = useState('');
     const [userId, setUserId] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    const [groupName, setGroupName] = useState("");
+    const [groupDesc, setGroupDesc] = useState("");
+    const [groupId, setGroupId] = useState("");
+
     var token = JSON.parse(localStorage.getItem('token'));
     var id = JSON.parse(localStorage.getItem('id'));
     const navigate = useNavigate();
@@ -34,14 +41,14 @@ function Admin() {
 
     useEffect(() => {
         setIsLoading(false);
-        async function getAdmin(){
+        async function getAdmin() {
             await api.get(`user/${id}`)
-            .then((res)=>{
-                if(res.data.admin!= true){
-                    navigate(`../feed`);
-                }
-            })
-            
+                .then((res) => {
+                    if (res.data.admin != true) {
+                        navigate(`../feed`);
+                    }
+                })
+
         }
         getAdmin();
 
@@ -64,6 +71,24 @@ function Admin() {
         e.preventDefault();
         setDeleteOpen(false);
         deleteUser();
+    }
+
+    function handleGroupCreate(e) {
+        e.preventDefault();
+        setcreateGOpen(false);
+        createGroup();
+    }
+
+    function handleGroupUpdate(e) {
+        e.preventDefault();
+        setUpdateGOpen(false);
+        updateGroup();
+    }
+
+    function handleGroupDelete(e) {
+        e.preventDefault();
+        setDeleteOpen(false);
+        deleteGroup();
     }
 
     function handleLogs(e) {
@@ -138,6 +163,43 @@ function Admin() {
         await api.delete(`user/${userId}`, config)
             .then(() => {
                 alert('Usuário deletado');
+            })
+    }
+
+    async function createGroup() {
+        var body = {
+            nome: groupName,
+            descricao: groupDesc,
+        }
+
+        await api.post(`group`, body)
+            .then(() => {
+                alert('Grupo criado');
+            })
+    }
+
+    async function updateGroup() {
+        var body = {
+            nome: groupName,
+            descricao: groupDesc,
+        }
+        var bodyTeste = {}
+        if (body.nome !== "") {
+            bodyTeste.nome = body.nome
+        }
+        if (body.descricao !== "") {
+            bodyTeste.descricao = body.descricao
+        }
+        await api.put(`group/${groupId}`, bodyTeste, config)
+            .then(() => {
+                alert('Grupo atualizado');
+            })
+    }
+
+    async function deleteGroup() {
+        await api.delete(`group/${groupId}`, config)
+            .then(() => {
+                alert('Grupo deletado');
             })
     }
 
@@ -222,53 +284,41 @@ function Admin() {
                 </div>
                 <div className='crud-grupo'>
                     <h4>CRUD Grupo</h4>
-                    <div className='card-create'>
-                        <button className='toggle' onClick={() => setFormOpen(!formOpen)}>Criar grupo</button>
-                        <div className={formOpen ? "content show" : "content"}>
-                            <form onSubmit={handleCreate} className='admin-form'>
-                                <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
-                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
-                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
-                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
-                                <label className='admin-bool'>Usuário é administrador?
-                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
-                                </label>
+                    <div className='card-create-grupo'>
+                        <button className='toggle-grupo' onClick={() => setcreateGOpen(!createGOpen)}>Criar grupo</button>
+                        <div className={createGOpen ? "content-grupo show" : "content-grupo"}>
+                            <form onSubmit={handleGroupCreate} className='admin-form-grupo'>
+                                <input type="text" placeholder="Nome do grupo" value={groupName} onChange={(e) => setGroupName(e.target.value)} />
+                                <div className='desc'>
+                                    <input type="text" placeholder="Descrição" value={groupDesc} onChange={(e) => setGroupDesc(e.target.value)} />
+                                </div>
                             </form>
-                            <button className='form-button' type="submit" onClick={handleCreate}>Salvar</button>
+                            <button className='form-button-grupo' type="submit" onClick={handleGroupCreate}>Salvar</button>
                         </div>
                     </div>
-                    <div className='card-update'>
-                        <button className='toggle-update' onClick={() => setUpdateOpen(!updateOpen)}>Atualizar grupo</button>
-                        <div className={updateOpen ? "update show" : "update"}>
-                            <form onSubmit={handleUpdate} className='update-form'>
-                                <input type="text" placeholder="ID do usuário" value={userId} onChange={(e) => setUserId(e.target.value)} />
-                                <input type="text" placeholder="Nome do usuário" value={nome} onChange={(e) => setNome(e.target.value)} />
-                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
-                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
-                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
-                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
-                                <label className='update-bool'>Usuário é administrador?
-                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
-                                </label>
+                    <div className='card-update-grupo'>
+                        <button className='toggle-update-grupo' onClick={() => setUpdateGOpen(!updateGOpen)}>Atualizar grupo</button>
+                        <div className={updateGOpen ? "update-grupo show" : "update-grupo"}>
+                            <form onSubmit={handleGroupUpdate} className='update-form-grupo'>
+                            <input type="text" placeholder="ID do grupo" value={groupId} onChange={(e) => setGroupId(e.target.value)} />
+                                <input type="text" placeholder="Nome do grupo" value={groupName} onChange={(e) => setGroupName(e.target.value)} />
+                                <div className='desc'>
+                                    <input type="text" placeholder="Descrição" value={groupDesc} onChange={(e) => setGroupDesc(e.target.value)} />
+                                </div>
                             </form>
-                            <button className='update-button' type="submit" onClick={handleUpdate}>Salvar</button>
+                            <button className='update-button-grupo' type="submit" onClick={handleGroupUpdate}>Salvar</button>
                         </div>
                     </div>
                     <div className='card-delete'>
-                        <button className='toggle-delete' onClick={() => setDeleteOpen(!deleteOpen)}>Deletar grupo</button>
-                        <div className={deleteOpen ? "delete show" : "delete"}>
-                            <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
-                            <button className='delete-button' type="submit" onClick={handleDelete}>Salvar</button>
+                        <button className='toggle-delete' onClick={() => setDeleteGOpen(!deleteGOpen)}>Deletar grupo</button>
+                        <div className={deleteGOpen ? "delete show" : "delete"}>
+                            <input type='text' placeholder='ID do grupo' value={groupId} onChange={(e) => setGroupId(e.target.value)}></input>
+                            <button className='delete-button' type="submit" onClick={handleGroupDelete}>Salvar</button>
                         </div>
                     </div>
                 </div>
                 <div className='desafios'>
-                <h4>Pesquisas</h4>
+                    <h4>Pesquisas</h4>
                     <div className='card-logs'>
                         <button className='toggle-logs' onClick={() => setLogsOpen(!logsOpen)}>Pesquisar entradas do usuário</button>
                         <div className={logsOpen ? "logs show" : "logs"}>
