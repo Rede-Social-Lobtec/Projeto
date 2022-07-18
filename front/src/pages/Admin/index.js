4d8b31721f2f3b9660f1826bc097303ec53ace9e
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-18 00:02:57 -0300

    Mudanças de design na tela de admin
    Mudanças de design na tela de admin

--- front/src/pages/Admin/index.js
+++ front/src/pages/Admin/index.js
@@ -162,78 +162,126 @@ function Admin() {
                 <div className='nav-bar'>
                     <h1>Página do administrador</h1>
                     <img src={require('../../assets/logo.png')}></img>
-
-                    <div className='crud'>
-                        <div className='card-create'>
-                            <button className='toggle' onClick={() => setFormOpen(!formOpen)}>Criar usuário</button>
-                            <div className={formOpen ? "content show" : "content"}>
-                                <form onSubmit={handleCreate} className='admin-form'>
-                                    <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-                                    <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
-                                    <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
-                                    <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
-                                    <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
-                                    <label className='admin-bool'>Usuário é administrador?
-                                        <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
-                                    </label>
-                                </form>
-                                <button className='form-button' type="submit" onClick={handleCreate}>Salvar</button>
-                            </div>
+                </div>
+                <div className='crud'>
+                    <h4>CRUD Usuário</h4>
+                    <div className='card-create'>
+                        <button className='toggle' onClick={() => setFormOpen(!formOpen)}>Criar usuário</button>
+                        <div className={formOpen ? "content show" : "content"}>
+                            <form onSubmit={handleCreate} className='admin-form'>
+                                <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
+                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
+                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
+                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
+                                <label className='admin-bool'>Usuário é administrador?
+                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
+                                </label>
+                            </form>
+                            <button className='form-button' type="submit" onClick={handleCreate}>Salvar</button>
                         </div>
-                        <div className='card-update'>
-                            <button className='toggle-update' onClick={() => setUpdateOpen(!updateOpen)}>Atualizar usuário</button>
-                            <div className={updateOpen ? "update show" : "update"}>
-                                <form onSubmit={handleUpdate} className='update-form'>
-                                    <input type="text" placeholder="ID do usuário" value={userId} onChange={(e) => setUserId(e.target.value)} />
-                                    <input type="text" placeholder="Nome do usuário" value={nome} onChange={(e) => setNome(e.target.value)} />
-                                    <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
-                                    <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
-                                    <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
-                                    <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
-                                    <label className='update-bool'>Usuário é administrador?
-                                        <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
-                                    </label>
-                                </form>
-                                <button className='update-button' type="submit" onClick={handleUpdate}>Salvar</button>
-                            </div>
-                        </div>
-                        <div className='card-delete'>
-                            <button className='toggle-delete' onClick={() => setDeleteOpen(!deleteOpen)}>Deletar usuário</button>
-                            <div className={deleteOpen ? "delete show" : "delete"}>
-                                <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
-                                <button className='delete-button' type="submit" onClick={handleDelete}>Salvar</button>
-                            </div>
-                        </div>
-
                     </div>
-                    <div className='desafios'>
-                        <div className='card-logs'>
-                            <button className='toggle-logs' onClick={() => setLogsOpen(!logsOpen)}>Pesquisar entradas do usuário</button>
-                            <div className={logsOpen ? "logs show" : "logs"}>
-                                <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
-                                <input type='date' placeholder='data' value={data} onChange={(e) => setData(e.target.value)}></input>
-                                <div className='log-res'>
-                                    <button className='logs-button' type="submit" onClick={handleLogs}>Pesquisar</button>
-                                    {logsRes && <p>Numero de entrada para este usuário: {logsRes}</p>}
-                                </div>
+                    <div className='card-update'>
+                        <button className='toggle-update' onClick={() => setUpdateOpen(!updateOpen)}>Atualizar usuário</button>
+                        <div className={updateOpen ? "update show" : "update"}>
+                            <form onSubmit={handleUpdate} className='update-form'>
+                                <input type="text" placeholder="ID do usuário" value={userId} onChange={(e) => setUserId(e.target.value)} />
+                                <input type="text" placeholder="Nome do usuário" value={nome} onChange={(e) => setNome(e.target.value)} />
+                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
+                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
+                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
+                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
+                                <label className='update-bool'>Usuário é administrador?
+                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
+                                </label>
+                            </form>
+                            <button className='update-button' type="submit" onClick={handleUpdate}>Salvar</button>
+                        </div>
+                    </div>
+                    <div className='card-delete'>
+                        <button className='toggle-delete' onClick={() => setDeleteOpen(!deleteOpen)}>Deletar usuário</button>
+                        <div className={deleteOpen ? "delete show" : "delete"}>
+                            <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
+                            <button className='delete-button' type="submit" onClick={handleDelete}>Salvar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='crud-grupo'>
+                    <h4>CRUD Grupo</h4>
+                    <div className='card-create'>
+                        <button className='toggle' onClick={() => setFormOpen(!formOpen)}>Criar grupo</button>
+                        <div className={formOpen ? "content show" : "content"}>
+                            <form onSubmit={handleCreate} className='admin-form'>
+                                <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
+                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
+                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
+                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
+                                <label className='admin-bool'>Usuário é administrador?
+                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
+                                </label>
+                            </form>
+                            <button className='form-button' type="submit" onClick={handleCreate}>Salvar</button>
+                        </div>
+                    </div>
+                    <div className='card-update'>
+                        <button className='toggle-update' onClick={() => setUpdateOpen(!updateOpen)}>Atualizar grupo</button>
+                        <div className={updateOpen ? "update show" : "update"}>
+                            <form onSubmit={handleUpdate} className='update-form'>
+                                <input type="text" placeholder="ID do usuário" value={userId} onChange={(e) => setUserId(e.target.value)} />
+                                <input type="text" placeholder="Nome do usuário" value={nome} onChange={(e) => setNome(e.target.value)} />
+                                <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="text" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="text" placeholder="Departametno" value={departamento} onChange={(e) => setDepartament(e.target.value)} />
+                                <input type="text" placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
+                                <input type="text" placeholder="Data de Nascimento" value={data_nascimento} onChange={(e) => setNascimento(e.target.value)} />
+                                <input type="text" placeholder="Telefone" value={telefone} onChange={(e) => setTelfone(e.target.value)} />
+                                <label className='update-bool'>Usuário é administrador?
+                                    <input type='checkbox' value={adminBool} onChange={(e) => setAdminBool(true)}></input>
+                                </label>
+                            </form>
+                            <button className='update-button' type="submit" onClick={handleUpdate}>Salvar</button>
+                        </div>
+                    </div>
+                    <div className='card-delete'>
+                        <button className='toggle-delete' onClick={() => setDeleteOpen(!deleteOpen)}>Deletar grupo</button>
+                        <div className={deleteOpen ? "delete show" : "delete"}>
+                            <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
+                            <button className='delete-button' type="submit" onClick={handleDelete}>Salvar</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='desafios'>
+                <h4>Pesquisas</h4>
+                    <div className='card-logs'>
+                        <button className='toggle-logs' onClick={() => setLogsOpen(!logsOpen)}>Pesquisar entradas do usuário</button>
+                        <div className={logsOpen ? "logs show" : "logs"}>
+                            <input type='text' placeholder='id do usuario' value={userId} onChange={(e) => setUserId(e.target.value)}></input>
+                            <input type='date' placeholder='data' value={data} onChange={(e) => setData(e.target.value)}></input>
+                            <div className='log-res'>
+                                <button className='logs-button' type="submit" onClick={handleLogs}>Pesquisar</button>
+                                {logsRes && <p>Numero de entrada para este usuário: {logsRes}</p>}
                             </div>
                         </div>
-                        <div className='card-tema'>
-                            <button className='toggle-tema' onClick={() => setTemaOpen(!temaOpen)}>Pesquisar tema com mais interações</button>
-                            <div className={temaOpen ? "tema show" : "tema"}>
-                                <p className='guide'>Interações = Curtidas + Comentários</p>
-                                <div className='tema-res'>
-                                    <button className='tema-button' type="submit" onClick={handleTema}>Pesquisar</button>
-                                    {temaRes && <p>{temaRes.msg}</p>}
-                                </div>
+                    </div>
+                    <div className='card-tema'>
+                        <button className='toggle-tema' onClick={() => setTemaOpen(!temaOpen)}>Pesquisar tema com mais interações</button>
+                        <div className={temaOpen ? "tema show" : "tema"}>
+                            <p className='guide'>Interações = Curtidas + Comentários</p>
+                            <div className='tema-res'>
+                                <button className='tema-button' type="submit" onClick={handleTema}>Pesquisar</button>
+                                {temaRes && <p>{temaRes.msg}</p>}
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
