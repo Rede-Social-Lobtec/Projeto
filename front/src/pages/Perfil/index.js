6433c4d9126b3d8316401b1764d2b6095c8a81e1
Lucas Lima <lucasl1m4x@gmail.com>
2022-07-19 14:24:37 -0300

    schimbări
    schimbări

--- front/src/pages/Perfil/index.js
+++ front/src/pages/Perfil/index.js
@@ -1,10 +1,12 @@
 import './perfil.css';
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link} from 'react-router-dom';
 import Header from '../../components/Header';
 import Modal from '../../components/Modal';
 import avatar from '../../assets/no-photo.png';
+import { AiFillLike, AiOutlineComment, AiFillStar} from 'react-icons/ai';
+const loadingGIF = require('../../assets/loading.gif');
 
 function Perfil() {
 
@@ -16,6 +18,7 @@ function Perfil() {
   const [grupo, setGrupo] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [follow, setFollow] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   var idUser = localStorage.getItem('id');
 
   const navigate = useNavigate();
@@ -63,9 +66,10 @@ function Perfil() {
           Authorization: `Bearer ${token}`
         }
       };
-      await api.get(`group/${id}/posts`, config)
+      await api.get(`postsUser/${id}`, config)
         .then((res) => {
           setPost(res.data);
+          setLoaded(true);
           if (res.data.msg == "Apenas usuários membros do grupo podem ver as publicações.") {
           }
         })
@@ -79,22 +83,24 @@ function Perfil() {
   async function seguirUser() {
     var body = { "id_user": id }
     const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
     };
 
     await api.put(`user`, body, config)
-        .then((res) => {
-            console.log(res.data.msg);
-            if(res.data.msg == 'Você seguiu o usuário!'){
-                setFollow(true);
-            }
-            if(res.data.msg == 'Você não está mais seguindo este usuário!'){
-                setFollow(false);
-            }
-        })
-}
+      .then((res) => {
+        console.log(res.data.msg);
+        if (res.data.msg == 'Você seguiu o usuário!') {
+          setFollow(true);
+          alert('Você seguiu o usuário');
+        }
+        if (res.data.msg == 'Você não está mais seguindo este usuário!') {
+          setFollow(false);
+          alert('Você deixou de seguir o usuário');
+        }
+      })
+  }
 
   function verUser(id) {
     navigate(`../perfil/${id}`);
@@ -115,19 +121,18 @@ function Perfil() {
             <p>Aniversário:{user.data_nascimento}</p>
           </div>
           <div className='perfil-seguir'>
-          {idUser != JSON.stringify(id) && follow ? <button key={id} onClick={seguirUser}>Seguindo</button>:
-            <button key={id}  onClick={seguirUser}>Seguir</button>}
+            {idUser != JSON.stringify(id) && <button key={id} onClick={seguirUser}>Seguir</button>}
           </div>
           {idUser == JSON.stringify(id) &&
             <div className='perfil-editar'>
-              <button className='bota-editar' onClick={() => {setModalOpen(true)}}>
+              <button className='bota-editar' onClick={() => { setModalOpen(true) }}>
                 Editar
-            </button>
+              </button>
             </div>
           }
-            {modalOpen && <Modal setOpenModal={setModalOpen}/>}
-        
-          
+          {modalOpen && <Modal setOpenModal={setModalOpen} />}
+
+
         </div>
         <div className='perfil-list'>
           <p className='list'>Seguindo...</p>
@@ -185,7 +190,66 @@ function Perfil() {
 
       </div>
       <div className='perfil-posts'>
-        <h3>Posts</h3>
+        <h1>Posts</h1>
+        <div className='div-posts perfil'>
+          <ul>
+            {!loaded &&
+              <div>
+                <h3>Carregando posts... <img src={loadingGIF} className="loading-gif" /></h3>
+              </div>
+            }
+            {loaded && post.length == 0 && <h3>Ainda não há publicações!</h3>}
+            {loaded && post.length > 0 && post.map((p) => {
+              var arrayDataHora = p.data.split(" ");
+              var data = arrayDataHora[0];
+              var hora = arrayDataHora[1];
+              hora = hora.slice(0, hora.length - 3);
+
+              return (
+                <li key={p._id} className="div-post">
+                  <div className='post-header'>
+                    <Link to={`../perfil/${id}`}>
+                      {user.foto != undefined ?
+                        <img src={user.foto} alt="foto" className='img-user' />
+                        :
+                        <img src={avatar} alt="foto" className='img-user' />
+                      }
+                      <div>
+                        {user != undefined ?
+                          <strong>{user.nome}</strong>
+                          :
+                          <strong>-- Nome usuário</strong>
+                        }
+                        <p>{data} às {hora}</p>
+                      </div>
+                    </Link>
+                    {user.admin && <AiFillStar color="#670067" />}
+                  </div>
+                  <div className='post-content'>
+                    <p>{p.descricao}</p>
+                  </div>
+                  <div className='post-footer'>
+                    <div className='div-interacoes-post'>
+                      <div className='div-total-likes'>
+                        <Link to={`/detalhePost/${p._id}`}>
+                          <AiFillLike color="#727272" className='total-likes-icon' />
+                          {p.curtidaDetalhe.length}
+                        </Link>
+                      </div>
+
+                      <div className='div-post-comments'>
+                        <Link to={`/detalhePost/${p._id}`}>
+                          <p>ver comentários</p>
+                          <AiOutlineComment color="#727272" className='post-comments-icon' />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
       <Header />
     </div>
